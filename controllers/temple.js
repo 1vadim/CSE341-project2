72751a93a9f7aa12924d1d518ab8cffbe6b6ebc7
1vadim <vchemariev@byupathway.edu>
2026-07-08 16:18:53 +0200

    w02-Team-v3

--- controllers/temple.js
+++ controllers/temple.js
@@ -64,99 +64,100 @@ exports.findAll = (req, res) => {
 // Find a single Temple with an id
 exports.findOne = (req, res) => {
   const temple_id = req.params.temple_id;
-  if (req.header('apiKey') === apiKey) {
+  // if (req.header('apiKey') === apiKey)
+  if (true) {
     Temple.find({ temple_id: temple_id })
       .then((data) => {
         if (!data)
           res
             .status(404)
-            .send({ message: 'Not found Temple with id ' + temple_id });
+            .send({ message: "Not found Temple with id " + temple_id });
         else res.send(data[0]);
       })
       .catch((err) => {
         res.status(500).send({
-          message: 'Error retrieving Temple with temple_id=' + temple_id,
+          message: "Error retrieving Temple with temple_id=" + temple_id,
         });
       });
   } else {
-    res.send('Invalid apiKey, please read the documentation.');
+    res.send("Invalid apiKey, please read the documentation.");
   }
 };
 
-// // Update a Temple by the id in the request
-// exports.update = (req, res) => {
-//   if (!req.body) {
-//     return res.status(400).send({
-//       message: 'Data to update can not be empty!',
-//     });
-//   }
+// Update a Temple by the id in the request
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Data to update can not be empty!',
+    });
+  }
 
-//   const id = req.params.id;
+  const id = req.params.temple_id;
 
-//   Temple.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-//     .then((data) => {
-//       if (!data) {
-//         res.status(404).send({
-//           message: `Cannot update Temple with id=${id}. Maybe Temple was not found!`,
-//         });
-//       } else res.send({ message: 'Temple was updated successfully.' });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: 'Error updating Temple with id=' + id,
-//       });
-//     });
-// };
+  Temple.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Temple with id=${id}. Maybe Temple was not found!`,
+        });
+      } else res.send({ message: 'Temple was updated successfully.' });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Error updating Temple with id=' + id,
+      });
+    });
+};
 
-// // Delete a Temple with the specified id in the request
-// exports.delete = (req, res) => {
-//   const id = req.params.id;
+// Delete a Temple with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.temple_id;
 
-//   Temple.findByIdAndRemove(id)
-//     .then((data) => {
-//       if (!data) {
-//         res.status(404).send({
-//           message: `Cannot delete Temple with id=${id}. Maybe Temple was not found!`,
-//         });
-//       } else {
-//         res.send({
-//           message: 'Temple was deleted successfully!',
-//         });
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message: 'Could not delete Temple with id=' + id,
-//       });
-//     });
-// };
+  Temple.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Temple with id=${id}. Maybe Temple was not found!`,
+        });
+      } else {
+        res.send({
+          message: 'Temple was deleted successfully!',
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: 'Could not delete Temple with id=' + id,
+      });
+    });
+};
 
-// // Delete all Temples from the database.
-// exports.deleteAll = (req, res) => {
-//   Temple.deleteMany({})
-//     .then((data) => {
-//       res.send({
-//         message: `${data.deletedCount} Temples were deleted successfully!`,
-//       });
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message:
-//           err.message || 'Some error occurred while removing all temple.',
-//       });
-//     });
-// };
+// Delete all Temples from the database.
+exports.deleteAll = (req, res) => {
+  Temple.deleteMany({})
+    .then((data) => {
+      res.send({
+        message: `${data.deletedCount} Temples were deleted successfully!`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while removing all temple.',
+      });
+    });
+};
 
-// // Find all published Temples
-// exports.findAllPublished = (req, res) => {
-//   Temple.find({ published: true })
-//     .then((data) => {
-//       res.send(data);
-//     })
-//     .catch((err) => {
-//       res.status(500).send({
-//         message:
-//           err.message || 'Some error occurred while retrieving temple.',
-//       });
-//     });
-// };
+// Find all published Temples
+exports.findAllPublished = (req, res) => {
+  Temple.find({ published: true })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || 'Some error occurred while retrieving temple.',
+      });
+    });
+};
